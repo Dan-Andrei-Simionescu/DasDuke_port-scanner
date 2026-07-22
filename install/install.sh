c7c8af50a1ebe00c65f2b06554309740a554fb1d
Dan-Andrei-Simionescu <andreidas04@gmail.com>
2026-07-22 19:18:30 +0300

    Modified commands in install.sh.

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Installing DasDuke Port Scanner..."
-
 INSTALL_DIR="$(pwd)"
 
 echo '#!/bin/bash' > scan_temp
@@ -9,6 +7,9 @@ echo "python3 \"${INSTALL_DIR}/src/port-scanner.py\" \"\$@\"" >> scan_temp
 
 sudo mv scan_temp /usr/local/bin/scan
 sudo chmod +x /usr/local/bin/scan
+
+echo "Installing DasDuke Port Scanner..."
+sleep 3s
 
 echo "Installation complete!"
 echo "You can now run 'scan -t <IP_TARGET> -p <PORT_RANGE>' from anywhere in your terminal."
